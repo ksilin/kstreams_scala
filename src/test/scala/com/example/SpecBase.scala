decd68aab8586552c1c2a8205466b9f26d33acba
ksilin <konstantin.siln@gmail.com>
2022-07-10 16:05:42 +0200

    SpecBase - add caching limit of 10M, set commit interval to 100 ms

--- src/test/scala/com/example/SpecBase.scala
+++ src/test/scala/com/example/SpecBase.scala
@@ -18,6 +18,9 @@ class SpecBase extends AnyFreeSpecLike with LogSupport with Matchers {
   streamsConfiguration.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "DUMMY_CONFIG")
   streamsConfiguration.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass)
   streamsConfiguration.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass)
+  // allowing caching, but putting upper bound on the time records remain in cache
+  streamsConfiguration.put(StreamsConfig.CACHE_MAX_BYTES_BUFFERING_CONFIG, 10 * 1024 * 1024L)
+  streamsConfiguration.put(StreamsConfig.COMMIT_INTERVAL_MS_CONFIG, 100)
   // Use a temporary directory for storing state, which will be automatically removed after the test.
   streamsConfiguration.put(
     StreamsConfig.STATE_DIR_CONFIG,
