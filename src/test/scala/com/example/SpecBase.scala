cec86569cea26d2eba98de2d66a92b1b13ee0fdb
ksilin <konstantin.siln@gmail.com>
2022-07-17 14:46:43 +0200

    remove default serdes, forbid topology optimzation in SpecBase

--- src/test/scala/com/example/SpecBase.scala
+++ src/test/scala/com/example/SpecBase.scala
@@ -13,14 +13,13 @@ import java.util.Properties
 class SpecBase extends AnyFreeSpecLike with LogSupport with Matchers {
 
   val streamsConfiguration: Properties = new Properties()
-  private val suiteName1: String       = this.suiteName
-  streamsConfiguration.put(StreamsConfig.APPLICATION_ID_CONFIG, suiteName1)
+  streamsConfiguration.put(StreamsConfig.APPLICATION_ID_CONFIG, this.suiteName)
   streamsConfiguration.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "DUMMY_CONFIG")
-  streamsConfiguration.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass)
-  streamsConfiguration.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass)
+  // we want to see the topology exactly as we created it:
+  streamsConfiguration.put(StreamsConfig.TOPOLOGY_OPTIMIZATION_CONFIG, StreamsConfig.NO_OPTIMIZATION)
+  // streamsConfiguration.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass)
+  // streamsConfiguration.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass)
   // allowing caching, but putting upper bound on the time records remain in cache
-  streamsConfiguration.put(StreamsConfig.CACHE_MAX_BYTES_BUFFERING_CONFIG, 10 * 1024 * 1024L)
-  streamsConfiguration.put(StreamsConfig.COMMIT_INTERVAL_MS_CONFIG, 100)
   // Use a temporary directory for storing state, which will be automatically removed after the test.
   streamsConfiguration.put(
     StreamsConfig.STATE_DIR_CONFIG,
@@ -32,7 +31,7 @@ class SpecBase extends AnyFreeSpecLike with LogSupport with Matchers {
   ) // pipe events through immediately
   streamsConfiguration.put(
     StreamsConfig.COMMIT_INTERVAL_MS_CONFIG,
-    0
+    10
   ) // pipe events through immediately
 
   val builder: StreamsBuilder = new StreamsBuilder()
