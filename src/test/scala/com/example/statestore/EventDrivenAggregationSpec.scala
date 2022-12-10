4a9fdc4a62dec69cc4439263700d9affd943b25c
ksilin <konstantin.siln@gmail.com>
2022-12-10 11:14:16 +0100

    EventDrivenAggregation: add idleAggregationWarningProcessor, add topology naming, cleanup

--- src/test/scala/com/example/statestore/EventDrivenAggregationSpec.scala
+++ src/test/scala/com/example/statestore/EventDrivenAggregationSpec.scala
@@ -1,39 +1,44 @@
 package com.example.statestore
 
 import com.example.SpecBase
+import com.example.statestore.EventDrivenAggregationTopo.{
+  aggregationDeserializer,
+  idleAggregationWarningDeserializer
+}
 import org.apache.kafka.clients.consumer.ConsumerConfig
 import org.apache.kafka.common.serialization.Serdes
 import org.apache.kafka.streams._
 import org.apache.kafka.streams.state.KeyValueStore
 
 import java.time.{ Duration, Instant }
-import java.{ lang, util }
+import java.util
 import _root_.scala.jdk.CollectionConverters._
 import _root_.scala.util.Random
 
 class EventDrivenAggregationSpec extends SpecBase {
 
-  val inputTopicName        = "inputTopic"
-  val triggerInputTopicName = "triggerTopic"
-  val outputTopicName       = "outputTopic"
-  val storeName             = s"${suiteName}_store"
+  val machineDataTopicName       = "machineDataInputTopic"
+  val triggerInputTopicName      = "triggerInputTopic"
+  val aggregationResultTopicName = "aggregationResultTopic"
+  val idleWarningTopicName       = "idleWarningTopic"
+  val storeName                  = "aggregationResultStore"
 
   private val sensorIds: List[String]  = (1 to 10).map(_.toString).toList
   private val machineIds: List[String] = ('A' to 'B').map(_.toString).toList
 
   private val nowInstant: Instant = Instant.now()
-  val now: Long = nowInstant.toEpochMilli
-  val later = now + 10000
+  private val now: Long           = nowInstant.toEpochMilli
+  private val later: Long         = now + 10000
 
-  val timestamps = now.to(later, 1000L).toList
+  private val timestamps: List[Long] = now.to(later, 1000L).toList
 
-  val sensorData: List[KeyValue[String, SensorData]] =
+  private val sensorData: List[KeyValue[String, SensorData]] =
     sensorIds map { i: String =>
       val v = Random.nextInt(10)
       new KeyValue(i, SensorData(i, v, now + v))
     }
 
-  val machineData: List[KeyValue[String, MachineData]] = machineIds flatMap { machineId =>
+  private val machineData: List[KeyValue[String, MachineData]] = machineIds flatMap { machineId =>
     timestamps map { newTs =>
       val machineIdData =
         MachineData(machineId, machineId + "_customer", sensorData.map(_.value).asJava, newTs)
@@ -41,12 +46,13 @@ class EventDrivenAggregationSpec extends SpecBase {
     }
   }
 
-  val triggerDataStart: List[KeyValue[String, MachineTrigger]] = machineIds map { machineId =>
-    val trigger = MachineTrigger(machineId, machineId + "_before_1", machineId + "_after_1", now)
-    new KeyValue(machineId, trigger)
+  private val triggerDataStart: List[KeyValue[String, MachineTrigger]] = machineIds map {
+    machineId =>
+      val trigger = MachineTrigger(machineId, machineId + "_before_1", machineId + "_after_1", now)
+      new KeyValue(machineId, trigger)
   }
 
-  val triggerDataEnd: List[KeyValue[String, MachineTrigger]] = machineIds map { machineId =>
+  private val triggerDataEnd: List[KeyValue[String, MachineTrigger]] = machineIds map { machineId =>
     val trigger = MachineTrigger(machineId, machineId + "_after_1", machineId + "_after_2", later)
     new KeyValue(machineId, trigger)
   }
@@ -54,12 +60,13 @@ class EventDrivenAggregationSpec extends SpecBase {
   "must aggregate and react to triggers" in {
 
     val topology: Topology =
-      //EventDrivenAggregationTopo.createTopologyPAPI(inputTopicName, triggerInputTopicName, outputTopicName, storeName, "1")
-      EventDrivenAggregationTopo.createTopologyDSL(
-        builder,
-        inputTopicName,
+      EventDrivenAggregationTopo.createTopologyPAPI(
+        //EventDrivenAggregationTopo.createTopologyDSL(
+        //  builder,
+        machineDataTopicName,
         triggerInputTopicName,
-        outputTopicName,
+        aggregationResultTopicName,
+        idleWarningTopicName,
         storeName,
         "1",
         1000L
@@ -74,31 +81,41 @@ class EventDrivenAggregationSpec extends SpecBase {
 
     val inputTopicMachineData: TestInputTopic[String, MachineData] =
       topologyTestDriver.createInputTopic(
-        inputTopicName,
+        machineDataTopicName,
         Serdes.String().serializer(),
         EventDrivenAggregationTopo.machineDataSerializer
       )
 
-    val triggerTopic: TestInputTopic[String, MachineTrigger] = topologyTestDriver.createInputTopic(
-      triggerInputTopicName,
-      Serdes.String().serializer(),
-      EventDrivenAggregationTopo.triggerSerializer
-    )
+    val inputTopicTrigger: TestInputTopic[String, MachineTrigger] =
+      topologyTestDriver.createInputTopic(
+        triggerInputTopicName,
+        Serdes.String().serializer(),
+        EventDrivenAggregationTopo.triggerSerializer
+      )
 
-    val outputTopic: TestOutputTopic[String, lang.String] = topologyTestDriver.createOutputTopic(
-      outputTopicName,
-      Serdes.String().deserializer(),
-      Serdes.String().deserializer()
-    )
+    val aggregationResultOutputTopic: TestOutputTopic[String, SensorDataAggregation] =
+      topologyTestDriver.createOutputTopic(
+        aggregationResultTopicName,
+        Serdes.String().deserializer(),
+        aggregationDeserializer
+      )
+
+    val idleWarningOutputTopic: TestOutputTopic[String, IdleAggregationWarning] =
+      topologyTestDriver.createOutputTopic(
+        idleWarningTopicName,
+        Serdes.String().deserializer(),
+        idleAggregationWarningDeserializer
+      )
+
     // first batch of data to test for dropping the records if no aggregation available
     inputTopicMachineData.pipeKeyValueList(machineData.take(2).asJava)
     // trigger creating first aggregation
-    triggerTopic.pipeKeyValueList(triggerDataStart.asJava)
+    inputTopicTrigger.pipeKeyValueList(triggerDataStart.asJava)
     inputTopicMachineData.pipeKeyValueList(machineData.take(5).asJava)
     // TODO - change data processing config here
     inputTopicMachineData.pipeKeyValueList(machineData.drop(5).asJava)
     // trigger finishing first aggregation
-    triggerTopic.pipeKeyValueList(triggerDataEnd.asJava)
+    inputTopicTrigger.pipeKeyValueList(triggerDataEnd.asJava)
 
     topologyTestDriver.advanceWallClockTime(Duration.ofMillis(later - now))
     (1 to 5).foreach { _ =>
@@ -107,19 +124,25 @@ class EventDrivenAggregationSpec extends SpecBase {
       topologyTestDriver.advanceWallClockTime(Duration.ofSeconds(1))
     }
 
-    logger.info("resulting records:")
-    val outputRecords: util.Map[String, lang.String] = outputTopic.readKeyValuesToMap()
-    outputRecords.asScala.values foreach (r => logger.info(r))
+    info("resulting records:")
+    val outputRecords: util.Map[String, SensorDataAggregation] =
+      aggregationResultOutputTopic.readKeyValuesToMap()
+    outputRecords.asScala.values foreach (r => info(r))
+
+    info("warning records:")
+    val warningRecords: util.Map[String, IdleAggregationWarning] =
+      idleWarningOutputTopic.readKeyValuesToMap()
+    warningRecords.asScala.values foreach (r => info(r))
 
     // explicit store init
-    val store: KeyValueStore[String, lang.String] =
-      topologyTestDriver.getKeyValueStore[String, lang.String](storeName)
-    logger.info("store contents at test end (should contain aggregations with sentinel afters):")
-    val storeContents: List[KeyValue[String, lang.String]] = store.all().asScala.toList
-    storeContents foreach (r => logger.info(r))
+    val store: KeyValueStore[String, SensorDataAggregation] =
+      topologyTestDriver.getKeyValueStore[String, SensorDataAggregation](storeName)
+    info("store contents at test end (should contain aggregations with sentinel afters):")
+    val storeContents: List[KeyValue[String, SensorDataAggregation]] = store.all().asScala.toList
+    storeContents foreach (r => info(r))
 
     topologyTestDriver.metrics().asScala.foreach { case (k, v) =>
-      logger.info(s"metric ${k.name()}: ${v.metricValue()}")
+      info(s"metric ${k.name()}: ${v.metricValue()}")
     }
   }
 
--- src/test/scala/com/example/statestore/EventDrivenAggregationTopo.scala
+++ src/test/scala/com/example/statestore/EventDrivenAggregationTopo.scala
@@ -8,6 +8,7 @@ import org.apache.kafka.streams.processor.{ Cancellable, PunctuationType, Punctu
 import org.apache.kafka.streams.scala.serialization.Serdes.stringSerde
 import org.apache.kafka.streams.state.{ KeyValueStore, StoreBuilder, Stores }
 import org.apache.kafka.streams.Topology
+import org.apache.kafka.streams.kstream.Named
 import org.apache.kafka.streams.scala.StreamsBuilder
 import org.apache.kafka.streams.scala.kstream.{ Consumed, KStream, Produced }
 import org.apache.kafka.streams.scala.ImplicitConversions._
@@ -31,8 +32,18 @@ case class SensorDataAggregation(
     sensorSum: Int,
     ts: Long
 )
+case class IdleAggregationWarning(
+    machineName: String,
+    aggregation: SensorDataAggregation,
+    sentAt: Long
+)
+
 case class AggregationConfig(factorOldValue: Double = 1.0, factorNewValue: Double = 1.0)
-case class TriggerPunctuatorConfig(inactivityCheckPeriod: Long, warnTimeoutMs: Long, warnOnInactivity: Boolean = true)
+case class TriggerPunctuatorConfig(
+    inactivityCheckPeriod: Long,
+    warnTimeoutMs: Long,
+    warnOnInactivity: Boolean = true
+)
 
 object EventDrivenAggregationTopo extends StrictLogging {
 
@@ -54,29 +65,38 @@ object EventDrivenAggregationTopo extends StrictLogging {
   val aggregationSerde: Serde[SensorDataAggregation] =
     Serdes.serdeFrom(aggregationSerializer, aggregationDeserializer)
 
+  val idleAggregationWarningSerializer: Serializer[IdleAggregationWarning] =
+    new GsonSerializer[IdleAggregationWarning]
+  val idleAggregationWarningDeserializer: Deserializer[IdleAggregationWarning] =
+    new GsonDeserializer[IdleAggregationWarning](classOf[IdleAggregationWarning])
+  val idleAggregationWarningSerde: Serde[IdleAggregationWarning] =
+    Serdes.serdeFrom(idleAggregationWarningSerializer, idleAggregationWarningDeserializer)
+
   // cannot serialize Option with Gson -> replacing by sentinel value
   val dummyTrigger: MachineTrigger = MachineTrigger("PLACEHOLDER", "PLACEHOLDER", "PLACEHOLDER", 0L)
+
+  val dataInputSourceName                 = "dataInput"
+  val triggerInputSourceName              = "triggerInput"
+  val dataProcessorName                   = "dataProcessor"
+  val triggerProcessorName                = "triggerProcessor"
+  val idleAggregationWarningProcessorName = "idleAggregationWarningProcessor"
 
   def createTopologyPAPI(
       inputTopic: String,
       triggerTopic: String,
-      outputTopic: String,
-      storeName: String,
+      aggregationResultTopic: String,
+      idleAggregationWarningTopic: String,
+      aggregationStoreName: String,
       sensorName: String,
       inactivityCheckPeriod: Long
   ): Topology = {
 
     val aggregationStoreBuilder: StoreBuilder[KeyValueStore[String, SensorDataAggregation]] =
       Stores.keyValueStoreBuilder(
-        Stores.persistentKeyValueStore(storeName),
+        Stores.persistentKeyValueStore(aggregationStoreName),
         stringSerde,
         aggregationSerde
       )
-
-    val dataInputSourceName    = "dataInput"
-    val triggerInputSourceName = "triggerInput"
-    val dataProcessorName      = "dataProcessor"
-    val triggerProcessorName   = "triggerProcessor"
 
     val topo = new Topology()
     topo
@@ -94,23 +114,46 @@ object EventDrivenAggregationTopo extends StrictLogging {
       )
       .addProcessor(
         dataProcessorName,
-        () => makeMachineDataProcessor(storeName, sensorName),
+        () => makeMachineDataProcessor(aggregationStoreName, sensorName),
         dataInputSourceName
       )
       .addProcessor(
         triggerProcessorName,
-        () => makeTriggerProcessor(storeName, inactivityCheckPeriod),
+        () => makeTriggerProcessor(aggregationStoreName, aggregationResultTopic),
         triggerInputSourceName
       )
-      .addStateStore(aggregationStoreBuilder, dataProcessorName, triggerProcessorName)
+      // HAS to have a parents although processes no data
+      // org.apache.kafka.streams.errors.TopologyException: Invalid topology: Processor idleAggregationWarningProcessor must have at least one parent
+      .addProcessor(
+        idleAggregationWarningProcessorName,
+        () =>
+          makeIdleAggregationWarningProcessor(
+            aggregationStoreName,
+            inactivityCheckPeriod,
+            idleAggregationWarningTopic
+          ),
+        triggerInputSourceName
+      )
+      .addStateStore(
+        aggregationStoreBuilder,
+        dataProcessorName,
+        triggerProcessorName,
+        idleAggregationWarningProcessorName
+      )
       .addSink(
-        outputTopic,
-        outputTopic,
+        aggregationResultTopic,
+        aggregationResultTopic,
         stringSerde.serializer(),
         aggregationSerializer,
         triggerProcessorName
       )
-
+      .addSink(
+        idleAggregationWarningTopic,
+        idleAggregationWarningTopic,
+        stringSerde.serializer(),
+        idleAggregationWarningSerializer,
+        idleAggregationWarningProcessorName
+      )
     topo
   }
 
@@ -118,60 +161,88 @@ object EventDrivenAggregationTopo extends StrictLogging {
       builder: StreamsBuilder,
       inputTopic: String,
       triggerTopic: String,
-      outputTopic: String,
-      storeName: String,
+      aggregationResultTopic: String,
+      idleAggregationWarningTopic: String,
+      aggregationStoreName: String,
       sensorName: String,
       inactivityCheckPeriod: Long
   ): Topology = {
 
     val aggregationStoreBuilder: StoreBuilder[KeyValueStore[String, SensorDataAggregation]] =
       Stores.keyValueStoreBuilder(
-        Stores.persistentKeyValueStore(storeName),
+        Stores.persistentKeyValueStore(aggregationStoreName),
         stringSerde,
         aggregationSerde
       )
     builder.addStateStore(aggregationStoreBuilder)
 
     val dataStream: KStream[String, MachineData] =
-      builder.stream(inputTopic)(Consumed.`with`(stringSerde, machineDataSerde))
+      builder.stream(inputTopic)(
+        Consumed.`with`(stringSerde, machineDataSerde).withName(dataInputSourceName)
+      )
     val triggerStream: KStream[String, MachineTrigger] =
-      builder.stream(triggerTopic)(Consumed.`with`(stringSerde, triggerSerde))
+      builder.stream(triggerTopic)(
+        Consumed.`with`(stringSerde, triggerSerde).withName(triggerInputSourceName)
+      )
 
     dataStream.process[String, SensorDataAggregation](
-      () => makeMachineDataProcessor(storeName, sensorName),
-      storeName
+      () => makeMachineDataProcessor(aggregationStoreName, sensorName),
+      Named.as(dataProcessorName),
+      aggregationStoreName
     )
     val aggregationStream =
       triggerStream.process[String, SensorDataAggregation](
-        () => makeTriggerProcessor(storeName, inactivityCheckPeriod),
-        storeName
+        () => makeTriggerProcessor(aggregationStoreName, aggregationResultTopic),
+        Named.as(triggerProcessorName),
+        aggregationStoreName
+      )
+    // cannot create JUST a punctuator, always need a processor
+    val warningStream: KStream[String, IdleAggregationWarning] =
+      triggerStream.process[String, IdleAggregationWarning](
+        () =>
+          makeIdleAggregationWarningProcessor(
+            aggregationStoreName,
+            inactivityCheckPeriod,
+            idleAggregationWarningTopic
+          ),
+        Named.as(idleAggregationWarningProcessorName),
+        aggregationStoreName
       )
 
-    aggregationStream.to(outputTopic)(Produced.`with`(stringSerde, aggregationSerde))
+    warningStream.to(idleAggregationWarningTopic)(
+      Produced
+        .`with`(stringSerde, idleAggregationWarningSerde)
+        .withName(idleAggregationWarningTopic)
+    )
+    aggregationStream.to(aggregationResultTopic)(
+      Produced.`with`(stringSerde, aggregationSerde).withName(aggregationResultTopic)
+    )
 
     builder.build()
   }
 
   def makeMachineDataProcessor(
-      storeName: String,
+      aggregationStoreName: String,
       sensorName: String,
   ): Processor[String, MachineData, String, SensorDataAggregation] =
     new Processor[String, MachineData, String, SensorDataAggregation] {
 
-      var ctx: ProcessorContext[String, SensorDataAggregation] = _
-      var store: KeyValueStore[String, SensorDataAggregation]  = _
+      var ctx: ProcessorContext[String, SensorDataAggregation]           = _
+      var aggregationStore: KeyValueStore[String, SensorDataAggregation] = _
 
       override def init(context: ProcessorContext[String, SensorDataAggregation]): Unit = {
         logger.info("initializing machine data processor - aggregator")
         ctx = context
-        store = ctx.getStateStore(storeName)
+        aggregationStore = ctx.getStateStore(aggregationStoreName)
       }
 
       override def process(record: Record[String, MachineData]): Unit = {
         val data      = record.value()
         val machineId = record.key()
         logger.info(s"processing data: $machineId $data")
-        Option(store.get(machineId)) map (c => updateAggregation(c, data)) getOrElse logger.warn(
+        Option(aggregationStore.get(machineId)) map (c =>
+          updateAggregation(c, data)
+        ) getOrElse logger.warn(
           s"no current aggregation found for $machineId and sensor $sensorName at ${record
             .timestamp()}. Ignoring event."
         )
@@ -184,14 +255,14 @@ object EventDrivenAggregationTopo extends StrictLogging {
         val maybeSensorValue: Option[Int] =
           newData.sensorData.asScala.find(sensor => sensorName == sensor.name).map(_.value)
 
-        // TODO - fetch configurable multiplication factor from config state store here
+        // TODO - fetch configurable multiplication factor from config state aggregationStore here
 
         maybeSensorValue map { value: Int =>
           val newAgg = currentAgg.copy(sensorSum = currentAgg.sensorSum + value, ts = newData.ts)
 
           logger.info(s"updating $value to current: ${currentAgg.sensorSum}")
           logger.info(s"current aggregation for ${currentAgg.name}: $newAgg")
-          store.put(currentAgg.name, newAgg)
+          aggregationStore.put(currentAgg.name, newAgg)
         } getOrElse logger.warn(
           s"no sensor value ford for sensor $sensorName in ${newData.name} with timestamp ${newData.ts}. Ignoring event"
         )
@@ -199,22 +270,66 @@ object EventDrivenAggregationTopo extends StrictLogging {
     }
 
   def makeTriggerProcessor(
-      storeName: String,
-      inactivityCheckPeriod: Long
+      aggregationStoreName: String,
+      aggregationResultTopicOut: String,
   ): Processor[String, MachineTrigger, String, SensorDataAggregation] =
     new Processor[String, MachineTrigger, String, SensorDataAggregation] {
 
-      var ctx: ProcessorContext[String, SensorDataAggregation] = _
-      var store: KeyValueStore[String, SensorDataAggregation]  = _
-      var cancellablePunctuator: Cancellable                   = _
-
-      // TODO - replace with value from config state store
-      var punctuatorConfig = TriggerPunctuatorConfig(1000, 3000)
+      var ctx: ProcessorContext[String, SensorDataAggregation]           = _
+      var aggregationStore: KeyValueStore[String, SensorDataAggregation] = _
 
       override def init(context: ProcessorContext[String, SensorDataAggregation]): Unit = {
         logger.info("initializing change trigger processor")
         ctx = context
-        store = ctx.getStateStore(storeName)
+        aggregationStore = ctx.getStateStore(aggregationStoreName)
+      }
+
+      override def process(record: Record[String, MachineTrigger]): Unit = {
+        val trigger   = record.value()
+        val machineId = record.key()
+
+        logger.info(s"change event for ${trigger.name} detected: ")
+        logger.info(
+          s"processing change from ${trigger.before} to ${trigger.after} at ${trigger.ts} "
+        )
+
+        val currentValue: Option[SensorDataAggregation] = Option(aggregationStore.get(machineId))
+
+        currentValue foreach { currentAgg =>
+          val updatedAgg = currentAgg.copy(triggerStop = trigger)
+          val forwardedRecord: Record[String, SensorDataAggregation] =
+            new Record(machineId, updatedAgg, trigger.ts)
+          logger.info(s"forwarding aggregation $updatedAgg")
+          ctx.forward(forwardedRecord, aggregationResultTopicOut)
+        }
+        initStoreFor(machineId, trigger)
+      }
+
+      def initStoreFor(machineId: String, trigger: MachineTrigger): Unit = {
+        val newAggregation = SensorDataAggregation(machineId, trigger, dummyTrigger, 0, trigger.ts)
+        logger.info(s"initializing state store value for $machineId with $newAggregation")
+        aggregationStore.put(machineId, newAggregation)
+      }
+    }
+
+  def makeIdleAggregationWarningProcessor(
+      aggregationStoreName: String,
+      inactivityCheckPeriod: Long,
+      idleAggregationWarningTopicOut: String
+  ): Processor[String, MachineTrigger, String, IdleAggregationWarning] =
+    new Processor[String, MachineTrigger, String, IdleAggregationWarning] {
+
+      var ctx: ProcessorContext[String, IdleAggregationWarning]          = _
+      var aggregationStore: KeyValueStore[String, SensorDataAggregation] = _
+      var cancellablePunctuator: Cancellable                             = _
+
+      // TODO - replace with value from config state store
+      var punctuatorConfig = TriggerPunctuatorConfig(1000, 3000)
+
+      override def init(context: ProcessorContext[String, IdleAggregationWarning]): Unit = {
+        logger.info("initializing change trigger processor")
+        ctx = context
+        aggregationStore = ctx.getStateStore(aggregationStoreName)
 
         // TODO - make punctuator configurable through an additional config topic:
         // configure warn timeout or disable warn timeout
@@ -226,40 +341,29 @@ object EventDrivenAggregationTopo extends StrictLogging {
       }
 
       override def process(record: Record[String, MachineTrigger]): Unit = {
-        val trigger   = record.value()
-        val machineId = record.key()
-
-        logger.info(s"change event for ${trigger.name} detected: ")
-        logger.info(s"processing change from ${trigger.before} to ${trigger.after} at ${trigger.ts} ")
-
-        val currentValue: Option[SensorDataAggregation] = Option(store.get(machineId))
-
-        currentValue foreach { currentAgg =>
-          val updatedAgg = currentAgg.copy(triggerStop = trigger)
-          val forwardedRecord: Record[String, SensorDataAggregation] =
-            new Record(machineId, updatedAgg, trigger.ts)
-          logger.info(s"forwarding aggregation $updatedAgg")
-          ctx.forward(forwardedRecord)
-        }
-        initStoreFor(machineId, trigger)
-      }
-
-      def initStoreFor(machineId: String, trigger: MachineTrigger): Unit = {
-        val newAggregation = SensorDataAggregation(machineId, trigger, dummyTrigger, 0, trigger.ts)
-        logger.info(s"initializing state store value for $machineId with $newAggregation")
-        store.put(machineId, newAggregation)
+        // NOOP
       }
 
       val forwardIfNoTriggerForTooLongPunctuator: Punctuator = (ts: Long) => {
-        if(punctuatorConfig.warnOnInactivity) {
-          val warnForTheseInactiveAggregations = store.all.asScala.map{kv =>
-            (ts - kv.value.ts, kv)
-          }.filter( _._1 > punctuatorConfig.warnTimeoutMs).toList
-          logger.info(s"forwardIfNoTriggerForTooLongPunctuator at $ts found ${warnForTheseInactiveAggregations.size} idle aggregations to warn on")
+        if (punctuatorConfig.warnOnInactivity) {
+          val warnForTheseInactiveAggregations = aggregationStore.all.asScala
+            .map { kv =>
+              (ts - kv.value.ts, kv)
+            }
+            .filter(_._1 > punctuatorConfig.warnTimeoutMs)
+            .toList
+          logger.info(
+            s"forwardIfNoTriggerForTooLongPunctuator at $ts found ${warnForTheseInactiveAggregations.size} idle aggregations to warn on"
+          )
           warnForTheseInactiveAggregations foreach { case (idleTs, agg) =>
-            logger.warn(s"${agg.value.name} inactive for $idleTs ms - dispatching inactivity warning")
-            // TODO - forward to warn topic
-            // optionally, delete from store
+            logger.warn(
+              s"${agg.value.name} inactive for $idleTs ms - dispatching inactivity warning"
+            )
+            val warning = IdleAggregationWarning(agg.key, agg.value, ctx.currentSystemTimeMs())
+            val warningRecord: Record[String, IdleAggregationWarning] =
+              new Record[String, IdleAggregationWarning](agg.key, warning, warning.sentAt)
+            ctx.forward(warningRecord, idleAggregationWarningTopicOut)
+          // optionally, delete from store
           }
         }
       }
